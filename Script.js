fee39446bcc1041a816fd29e733d3453fbc2600f
nithin702 <princenithin542@gmail.com>
2026-07-03 20:00:02 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdEQU9fNNjzfo-OGV5o9p_CiS_PA_NJNw",
+  authDomain: "vpl-2k27.firebaseapp.com",
+  projectId: "vpl-2k27",
+  storageBucket: "vpl-2k27.firebasestorage.app",
+  messagingSenderId: "919265368604",
+  appId: "1:919265368604:web:41587c7dd08f4c5d991dd9",
+  measurementId: "G-YL6CQ36HV6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.querySelector("form");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const playerName = form.querySelector('input[placeholder="Player Name"]').value;
@@ -11,24 +32,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const mobile = form.querySelector('input[placeholder="Mobile Number"]').value;
     const transactionId = form.querySelector('input[placeholder="Transaction ID"]').value;
 
-    if (
-      playerName === "" ||
-      age === "" ||
-      village === "" ||
-      mobile === ""
-    ) {
-      alert("Please fill all required fields.");
-      return;
+    try {
+
+      await addDoc(collection(db, "registrations"), {
+        playerName,
+        age,
+        village,
+        mobile,
+        transactionId,
+        createdAt: new Date()
+      });
+
+      alert("Registration Successful!");
+      form.reset();
+
+    } catch (error) {
+      alert("Error: " + error.message);
     }
 
-    alert(
-      "Registration Submitted Successfully!\n\n" +
-      "Player: " + playerName +
-      "\nVillage: " + village +
-      "\nMobile: " + mobile
-    );
-
-    form.reset();
   });
 
 });
