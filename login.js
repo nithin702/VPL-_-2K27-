6b3604942ad05190b043ec4990ed4dd22897b237
nithin702 <princenithin542@gmail.com>
2026-07-08 10:36:23 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
 
+function login(){
+
+const password=document.getElementById("password").value;
+
+if(password==="VPL@2027"){
+
+localStorage.setItem("adminLogin","true");
+
+window.location.href="admin.html";
+
+}else{
+
+alert("Wrong Password!");
+
+}
+
+}
