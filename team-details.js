544a602a8ae0faba3c60a395acb0e14a23ace4dd
nithin702 <princenithin542@gmail.com>
2026-07-10 16:27:04 +0530

    Update team-details.js

--- team-details.js
+++ team-details.js
@@ -1,1 +1,38 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// ================= Firebase =================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdEQU9fNNjzfo-OGV5o9p_CiS_PA_NJNw",
+  authDomain: "vpl-2k27.firebaseapp.com",
+  projectId: "vpl-2k27",
+  storageBucket: "vpl-2k27.firebasestorage.app",
+  messagingSenderId: "919265368604",
+  appId: "1:919265368604:web:41587c7dd08f4c5d991dd9",
+  measurementId: "G-YL6CQ36HV6"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// ================= Elements =================
+
+const teamInfo = document.getElementById("teamInfo");
+const playersList = document.getElementById("playersList");
+
+const params = new URLSearchParams(window.location.search);
+
+const teamName = params.get("team");
+
+document.getElementById("backBtn").onclick = () => {
+
+    window.location.href = "teams.html";
+
+};
